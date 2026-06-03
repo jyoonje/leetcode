33a99acc2874626a106f659850145e20b291e6b8
jyoonje <150825231+jyoonje@users.noreply.github.com>
2026-06-04 00:30:03 +0900

    Time: 44 ms (5.05%), Space: 48.1 MB (5.4%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -4,7 +4,7 @@ public class Solution {
 
     if (numRows == 1) return s;
 
-    String[][] matrix = new String[numRows][s.length()];
+    char[][] matrix = new char[numRows][s.length()];
 
     int start = 0;
 
@@ -13,7 +13,7 @@ public class Solution {
 
     while(stringLength < s.length()){   // 문자열 끝날 때까지 반복
       while (start < numRows && stringLength < s.length()) {
-        matrix[start][columnIdx] = String.valueOf(s.charAt(stringLength));
+        matrix[start][columnIdx] = s.charAt(stringLength);
         start++;
         stringLength++;
       }
@@ -23,17 +23,16 @@ public class Solution {
       while (start > 0 && stringLength < s.length()) {
         start--;
         columnIdx++;
-        matrix[start][columnIdx] = String.valueOf(s.charAt(stringLength));
+        matrix[start][columnIdx] = s.charAt(stringLength);
         stringLength++;
       }
       start++;
     }
 
-    // 만들어진 2차원 배열에서, 문자열 전부 더하기
     StringBuilder sb = new StringBuilder();
     for (int i = 0; i < numRows; i++) {
       for (int j = 0; j < matrix[i].length; j++) {
-        if (matrix[i][j] != null) {
+        if (matrix[i][j] != '\0') {
           sb.append(matrix[i][j]);
         }
       }
